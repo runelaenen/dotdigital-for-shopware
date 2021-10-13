6211b6e5a5fe793f5085bee87685045663e25fca
Alastair Mucklow <amucklow@strangerpixel.com>
2021-10-13 16:05:12 +0100

    Define productData and remove dmPt condition

--- Resources/app/storefront/src/dotdigital-for-shopware/plugins/wbt.plugin.js
+++ Resources/app/storefront/src/dotdigital-for-shopware/plugins/wbt.plugin.js
@@ -7,18 +7,18 @@ export default class WbtPlugin extends Plugin {
          * @type string
          */
         profileId: '',
+        /**
+         * Product data (only product pages)
+         * @type {Object}
+         */
+        productData: {},
     };
 
     init() {
         (function(w,d,u,t,o,c){w['dmtrackingobjectname'] = o;c = d.createElement( t );c.async = 1;c.src = u;t = d.getElementsByTagName( t )[0];t.parentNode.insertBefore( c,t );w[o] = w[o] || function(){(w[o].q = w[o].q || []).push( arguments );};
         })( window, document, '//static.trackedweb.net/js/_dmptv4.js', 'script', 'dmPt' );
 
-        window.dmPt( 'create', this.options.profileId );
-
-        if (typeof this.options.productData !== 'undefined') {
-            window.dmPt('track', this.options.productData || {});
-        } else {
-            window.dmPt('track');
-        }
+        window.dmPt('create', this.options.profileId);
+        window.dmPt('track', this.options.productData);
     }
 }
